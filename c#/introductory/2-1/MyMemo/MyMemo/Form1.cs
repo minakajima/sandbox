eb314512779d2b941d6386b5e106a2993509eaf5
Mikio Nakajima <mikio.nakaji@gmail.com>
2015-12-29 22:36:43 +0900

    4 タイトルバーにプログラム名を表示する

--- c#/introductory/2-1/MyMemo/MyMemo/Form1.cs
+++ c#/introductory/2-1/MyMemo/MyMemo/Form1.cs
@@ -12,9 +12,15 @@ namespace MyMemo
 {
     public partial class Form1 : Form
     {
+        const string ApplicationName = "MyMemo";
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Text = ApplicationName;
         }
     }
 }
