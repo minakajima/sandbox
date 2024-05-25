527d3a449cceb3ea25a9fb6aed892f3c19c1304e
minakajima <168143653+minakajima@users.noreply.github.com>
2024-05-25 15:04:04 +0900

    メモリ範囲外アクセスするコード

--- c#/exception_test/Program.cs
+++ c#/exception_test/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+
+        char[] chars = new char[0];
+
+        char chars1 = chars[1];
+    }
+}
